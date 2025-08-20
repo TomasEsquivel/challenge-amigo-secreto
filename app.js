4aeb4213dc7176771a2fd6b332cedb3b63279dcb
TomasEsquivel <todmy8@gmail.com>
2025-08-20 14:14:23 -0300

    Creado agregar-amigo

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const listaAmigos = [];
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function limpiarCaja(){
+    document.querySelector('input').value = '';
+}
+
+function agregarAmigo(){
+    let nuevoAmigo = document.querySelector('input').value;
+     if(nuevoAmigo === null || nuevoAmigo === ""){
+        alert('Por favor ingrese un nombre válido');
+     }
+     else{
+        listaAmigos.push(nuevoAmigo.trim());
+     }
+     limpiarCaja();
+     return;
+}// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
