85ffa3d7cb498c8f4938cb507f76c07905ddbeb0
TomasEsquivel <todmy8@gmail.com>
2025-08-20 11:39:57 -0300

    creado el almacenamiento de nombres

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
+const listaAmigos = [];
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
